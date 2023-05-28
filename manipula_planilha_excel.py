4a3173d92b0b48c1018cd78c259bd10f13c1949d
Drarlian <witoredson@gmail.com>
2023-05-28 20:53:52 -0300

    Criando o módulo "manipula_planilha_excel.py" com a estrutura básica de cada função que estará presente no módulo.
    Futuramente as funções serão implementadas.

---
+++ manipula_planilha_excel.py
@@ -0,0 +1,30 @@
+from openpyxl import Workbook, load_workbook
+
+
+planilha = load_workbook('Arquivo.xlsx')
+aba_ativa = planilha.active
+print(aba_ativa)
+
+
+def pegar_dados_intervalo_planilha(intervalo: str):
+    pass
+
+
+def atualizar_dados_intervalo_planilha(intervalo: str):
+    pass
+
+def adicionar_dados_fim_planilha(intervalo: str):
+    pass
+
+
+def adicionar_dados_intervalo_planilha(intervalo: str):
+    pass
+
+
+def remover_dados_intervalo_planilha(intervalo: str):
+    pass
+
+
+def atualizar_formatacao_planilha():
+    pass
+
