2e82322fdc9fad3a0ed33ea28d3f53b087253e50
Drarlian <witoredson@gmail.com>
2023-05-28 21:05:27 -0300

    Alterando o nome do módulo "manipula_planilha.py" para "manipula_planilha_google.py".
    A alteração foi feita para que outras opções de manipulação de planilha possam ser adicionadas futuramente.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from manipula_planilha_google import *
 from raspagem_dados import *
 from analisa_dados import *  # -> Importando o "manipula_planilha" dentro de "analise_dados".
 
-
 def adicionar_acoes(lista_acoes: list, primeira_vez: bool = False) -> None:
     """
     Recebe uma lista de ações e adiciona os ativos da lista na planilha juntamente com seus indicadores.
