bb5ed26b5851c573db55a27a9d902dd3db376d95
Drarlian <witoredson@gmail.com>
2024-03-04 14:12:35 -0300

    Alterando o método http das rotas para POST.

--- AtivosAPI/endpoints.py
+++ AtivosAPI/endpoints.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import RaspagemDados.raspagem_dados as raspagem
+from typing import List
 
 app = FastAPI()
+
+
+class Ativos(BaseModel):
+    lista_ativos: list
 
 
 class Fii(BaseModel):
@@ -22,74 +27,67 @@ class Acao(BaseModel):
     variacao_12M: str
 
 
-@app.get('/fiis/{ativos}')
-def get_fiis(ativos: str):
-    if (',' not in ativos) and (len(ativos) < 6):
-        return {"message": "Os dados fornecidos estão incorretos!"}
+@app.post('/fiis')
+def get_fiis(ativos: Ativos):
+    for elemento in ativos.lista_ativos:
+        if len(elemento) < 6:
+            return {"message": "Os dados fornecidos estão incorretos!"}
 
-    ativos = ativos.split(',')
-
-    if len(ativos[0]) >= 6:
-        try:
-            ativos_inicio: list = raspagem.new_pegar_dados_ativo('fiis', ativos, False)
-        except:
-            return {"message": "Erro Interno"}
-        else:
-            if ativos_inicio[0] is None:
-                return {"message": "Algum fii foi informado incorretamente!"}
-            else:
-                ativos_final: list = []
-
-                for elemento in ativos_inicio:
-                    ativo_temporario: dict = {
-                        'tipo': 'fii',
-                        'ativo': elemento[0].upper(),
-                        'cotacao': elemento[1],
-                        'dy_12M': elemento[2],
-                        'pvp': elemento[3],
-                        'liquidez_diaria': elemento[4],
-                        'variacao_12M': elemento[5]
-                    }
-
-                    ativos_final.append(ativo_temporario.copy())
-                    ativo_temporario.clear()
-
-                return ativos_final
+    try:
+        ativos_inicio: list = raspagem.new_pegar_dados_ativo('fiis', ativos.lista_ativos, False)
+    except:
+        return {"message": "Erro Interno"}
     else:
-        return {"message": "Os dados estão incorretos!"}
-
-@app.get('/acoes/{ativos}')
-def get_acoes(ativos: str):
-    if (',' not in ativos) and (len(ativos) < 5):
-        return {"message": "Os dados fornecidos estão incorretos!"}
-
-    ativos = ativos.split(',')
-
-    if len(ativos[0]) >= 5:
-        try:
-            ativos_inicio: list = raspagem.new_pegar_dados_ativo('acoes', ativos, False)
-        except:
-            return {"message": "Erro Interno"}
+        if ativos_inicio[0] is None:
+            return {"message": "Algum fii foi informado incorretamente!"}
         else:
-            if ativos_inicio[0] is None:
-                return {"message": "Alguma ação foi informada incorretamente!"}
-            else:
-                ativos_final: list = []
+            ativos_final: list = []
 
-                for elemento in ativos_inicio:
-                    ativo_temporario = {
-                        'tipo': 'ação',
-                        'ativo': elemento[0].upper(),
-                        'cotacao': elemento[1],
-                        'variacao_12M': elemento[2],
-                        'pl': elemento[3],
-                        'pvp': elemento[4],
-                        'dy': elemento[5]
-                    }
+            for elemento in ativos_inicio:
+                ativo_temporario: dict = {
+                    'tipo': 'fii',
+                    'ativo': elemento[0].upper(),
+                    'cotacao': elemento[1],
+                    'dy_12M': elemento[2],
+                    'pvp': elemento[3],
+                    'liquidez_diaria': elemento[4],
+                    'variacao_12M': elemento[5]
+                }
 
-                    ativos_final.append(ativo_temporario.copy())
-                    ativo_temporario.clear()
+                ativos_final.append(ativo_temporario.copy())
+                ativo_temporario.clear()
 
-                return ativos_final
+            return ativos_final
+
+
+@app.post('/acoes')
+def get_acoes(ativos: Ativos):
+    for elemento in ativos.lista_ativos:
+        if len(elemento) < 5:
+            return {"message": "Os dados fornecidos estão incorretos!"}
+
+    try:
+        ativos_inicio: list = raspagem.new_pegar_dados_ativo('acoes', ativos.lista_ativos, False)
+    except:
+        return {"message": "Erro Interno"}
     else:
-        return {"message": "Os dados estão incorretos!"}
+        if ativos_inicio[0] is None:
+            return {"message": "Alguma ação foi informada incorretamente!"}
+        else:
+            ativos_final: list = []
+
+            for elemento in ativos_inicio:
+                ativo_temporario = {
+                    'tipo': 'ação',
+                    'ativo': elemento[0].upper(),
+                    'cotacao': elemento[1],
+                    'variacao_12M': elemento[2],
+                    'pl': elemento[3],
+                    'pvp': elemento[4],
+                    'dy': elemento[5]
+                }
+
+                ativos_final.append(ativo_temporario.copy())
+                ativo_temporario.clear()
+
+            return ativos_final
