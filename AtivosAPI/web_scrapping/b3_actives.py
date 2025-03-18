2a3f69bc826d7965910b55cc137b2d8c7b3dc2ae
Drarlian <witoredson@gmail.com>
2025-03-18 14:31:01 -0300

    Foi aprimorada a nova função para se obter dados sobre os ativos da bolsa de valores. Além disso, agora essa função também consegue analisar fundos imobiliários.

---
+++ AtivosAPI/web_scrapping/b3_actives.py
@@ -0,0 +1,132 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+
+def b3_actives_from_web(tipo_ativo: str, lista_ativos: list) -> dict:
+    """
+    Procura pelas informações do(s) ativo(s) informado(s) de forma rápida e otimizada.
+    Formato do Retorno:
+    [['ativo1', 'informacao1', 'informacao2'], ['ativo2', 'outra_informacao1', 'outra_informacao2']]
+    :param tipo_ativo: Tipo do ativo que será pesquisado. Opções: acoes | fiis
+    :param lista_ativos: Lista contendo os ativos. A lista deve conter apenas ativos do mesmo tipo.
+    :return: Retorna uma lista de dicionários contendo as informações dos ativos solicitados.
+    """
+    # tipo_ativo = acoes | fiis
+    if tipo_ativo not in ('acoes', 'fiis'):
+        return {"status": None, "message": "O tipo de ativo informado não existe ou não está disponível."}
+
+    lista_completa = []
+
+    lista_urls = []
+    for nome_ativo in lista_ativos:
+        lista_urls.append(f'https://investidor10.com.br/{tipo_ativo}/{nome_ativo}/')
+
+    # Navegador Chrome:
+    chrome_configs = webdriver.ChromeOptions()
+    chrome_configs.add_argument("--headless")
+
+    navegador = webdriver.Chrome(options=chrome_configs)
+    # navegador = webdriver.Chrome()
+
+    for indice, url in enumerate(lista_urls):
+        if indice == 0:
+            navegador.get(url)
+
+            navegador.implicitly_wait(5)
+        else:
+            # Abrir uma nova aba
+            navegador.execute_script("window.open('', '_blank');")
+
+            # Mudar para a segunda aba
+            navegador.switch_to.window(navegador.window_handles[indice])
+
+            # Abrir o segundo link e pegar dados
+            navegador.get(url)
+
+        soup = BeautifulSoup(navegador.page_source, 'html.parser')
+
+        # PEGANDO OS DADOS DO CARD INICIAL DA PÁGINA:
+        elementos_valores = soup.find_all('div', attrs={'class': '_card-body'})
+
+        informacoes_titulo = [elemento.find('span').text for elemento in elementos_valores if elemento.find('span')]
+
+        if len(informacoes_titulo) == 0:
+            return {"status": False, "message": "O ativo informado não existe ou não foi encontrado."}
+
+        informacoes_titulo.insert(0, lista_ativos[indice])
+
+        if tipo_ativo == 'acoes':
+            novas_informacoes = {
+                'TITULO': informacoes_titulo[0],
+                'COTAÇÃO': informacoes_titulo[1],
+                'VARIAÇÃO (12M)': informacoes_titulo[2],
+                'P/L': informacoes_titulo[3],
+                'P/VP': informacoes_titulo[4],
+                'DY (12M)': informacoes_titulo[5],
+            }
+
+            # PEGANDO OS DADOS DO CARD PRINCIPAL DE INFORMAÇÃO DA PÁGINA:
+            principais_informacoes = soup.find('div', attrs={'id': 'table-indicators'}).find_all('div', attrs={'class': 'cell'})
+
+            for elemento in principais_informacoes:
+                temp = elemento.find('div', attrs={'class': 'value'})
+                novas_informacoes[elemento.find('span').text.strip()] = temp.find('span').text.strip()
+
+            # PEGANDO AS INFORMAÇÕES ESPECIFICAS DA EMPRESA:
+            indicadores = soup.find('div', attrs={'id': 'table-indicators-company'}).find_all('div', attrs={'class': 'cell'})
+
+            for indicador in indicadores:
+                temp = indicador.find('span', attrs={'class': 'value'}).find('div', attrs={'class': 'detail-value'})
+
+                if temp is not None:
+                    novas_informacoes[indicador.find('span').text.strip()] = temp.text.strip()
+                else:
+                    novas_informacoes[indicador.find('span', attrs={'class': 'title'}).text.strip()] = indicador.find('span', attrs={'class': 'value'}).text.strip()
+
+            lista_completa.append(novas_informacoes.copy())
+            novas_informacoes.clear()
+
+        elif tipo_ativo == 'fiis':
+            novas_informacoes = {
+                'TÍTULO': informacoes_titulo[0],
+                'COTAÇÃO': informacoes_titulo[1],
+                'DY (12M)': informacoes_titulo[2],
+                'P/VP (12M)': informacoes_titulo[3],
+                'LIQUIDEZ DIÁRIA': informacoes_titulo[4],
+                'VARIAÇÃO (12M)': informacoes_titulo[5],
+            }
+
+            # PEGANDO OS DADOS DO CARD PRINCIPAL DE INFORMAÇÃO DA PÁGINA:
+            principais_informacoes = soup.find('div', attrs={'id': 'table-indicators'}).find_all('div', attrs={
+                'class': 'cell'})
+
+            for elemento in principais_informacoes:
+                temp = elemento.find('div', attrs={'class': 'desc'}).find('div', attrs={'class': 'value'})
+                novas_informacoes[
+                    elemento.find('div', attrs={'class': 'desc'}).find('span').text.strip()] = temp.find(
+                    'span').text.strip()
+
+            lista_completa.append(novas_informacoes.copy())
+            novas_informacoes.clear()
+
+    navegador.quit()
+    return {"status": True, "informations": lista_completa}
+
+
+if __name__ == '__main__':
+    import timeit
+    import cProfile
+
+    # temp_result = b3_actives_from_web('fiis', ['CPTS11', 'RBVA11', 'NSLU11', 'XPML11'])
+    # print(temp_result)
+
+    tempo = timeit.timeit(lambda: b3_actives_from_web(tipo_ativo='fiis', lista_ativos=['CPTS11', 'RBVA11', 'NSLU11', 'XPML11']), number=10)  # Executa 10 vezes
+    print(f"Tempo médio por execução: {tempo / 10:.6f} segundos")  # -> Tempo médio por execução: 14.038316 segundos
+
+    # cProfile.run('b3_actives_from_web(tipo_ativo="fiis", lista_ativos=["CPTS11", "RBVA11", "NSLU11", "XPML11"])')
+
+    # if temp_result['status']:
+    #     for item in temp_result['informations']:
+    #         for chave, valor in item.items():
+    #             print(f'{chave}: {valor}')
+    #         print('-' * 30)
